cbaa1f9b83cffdd5f5fb768f20f1ebf4dadb82ae
Fernando Cordero <fernan942@gmail.com>
2020-01-11 21:28:33 -0300

    el objeto h response y sus herramientas_mas inert y path_4

--- index.js
+++ index.js
@@ -2,24 +2,41 @@
 
 // Requerir el modulo de hapi (Framework)
 const Hapi = require('@hapi/hapi')
+const inert = require('inert') // extiende los metodos disponible en el objeto h
+const path = require('path') // nos permite definir una ubicación relativa para todos los routes de nuestro proyecto
 
 // Configurar el servidor de nuestra aplicación. En un contenedor (Docker) si marca error colocar 0.0.0.0 (todos)
 const server = Hapi.server({
     port: process.env.PORT || 3000,
-    host: 'localhost' 
+    host: 'localhost',
+    //definir desde donde va acceder a las rutas. relaviteTo (routes.files)
+    routes: {
+        files: {
+            relativeTo: path.join(__dirname, 'public')
+        }
+    } 
 })
 
-// Definicion de función para inicializar el proyecto. Intenamnete hay tareas asincronas
+// Definicion de función para inicializar el proyecto. Internamente hay tareas asincronas
 async function init() {
       // Definición de rutas (indicar el método HTTP, URL y controlador de ruta)
     server.route({
        method: 'GET',
-       path: '/',
+       path: '/home',
        handler: (req, h) => {
            // El objeto h es un conjunto de utilidades para la respuesta.
            return h.response('Hola Mundo...!!!').code(200)
        } 
     })
+
+    server.route({
+        method: 'GET',
+        path: '/redirect',
+        handler: (req, h) => {
+            // El objeto h es un conjunto de utilidades para la respuesta.
+            return h.redirect('http://platzi.com')
+        } 
+     })
   // Arrancar el servidor de HapiJS, se considera una tarea asincrona.
     try {
         await server.start()
