546b3cf97efe719763efc5dc148f60dc996d7b43
Fernando Cordero <fernan942@gmail.com>
2020-01-11 21:38:02 -0300

    Uso de plugins_Contenido estatico_5

--- index.js
+++ index.js
@@ -19,26 +19,32 @@ const server = Hapi.server({
 
 // Definicion de función para inicializar el proyecto. Internamente hay tareas asincronas
 async function init() {
-      // Definición de rutas (indicar el método HTTP, URL y controlador de ruta)
-    server.route({
-       method: 'GET',
-       path: '/home',
-       handler: (req, h) => {
-           // El objeto h es un conjunto de utilidades para la respuesta.
-           return h.response('Hola Mundo...!!!').code(200)
-       } 
-    })
 
-    server.route({
-        method: 'GET',
-        path: '/redirect',
-        handler: (req, h) => {
-            // El objeto h es un conjunto de utilidades para la respuesta.
-            return h.redirect('http://platzi.com')
-        } 
-     })
   // Arrancar el servidor de HapiJS, se considera una tarea asincrona.
     try {
+        // registrando inert
+        await server.register(inert)
+
+              // Definición de rutas (indicar el método HTTP, URL y controlador de ruta)
+    server.route({
+        method: 'GET',
+        path: '/home',
+        handler: (req, h) => {
+            // El objeto h es un conjunto de utilidades para la respuesta.
+            return h.file('index.html')
+        } 
+     })
+ 
+     server.route({
+         method: 'GET',
+         path: '/{param*}',
+         handler: {
+             directory: {
+                 path: '.',
+                 index: ['index.html']
+             }
+         }
+      })
         await server.start()
     } catch (error) {
         console.error(error)
